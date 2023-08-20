627ec921765f46484a3e65f897a28a9fdab86bf0
XYD6268 <142665006+XYD6268@users.noreply.github.com>
2023-08-20 17:09:31 +0800

    0.1

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
-/*
- * 整个JS代码将在服务器刚启动时加载，请务必牢记：大部分与游戏相关的内容，
- * 如监听事件，生成物品，操作地图，控制生物等等都不能在刚开始的时候进行！
- * 
- * The entire JS code will be loaded when the server is just started. 
- * Please keep in mind that most game related content, such as monitoring events, 
- * generating items, operating worlds, controlling entities, etc., cannot be carried out at the beginning!
- */
-console.log("Wow world")
+const server = require('nukkit').server;
 
-/*
- * main函数将会在PNX服务器初始化完成后被调用，此函数被调用时及以后您可以进行与游戏相关的操作。
-
- * The main function will be called after the PNX server is initialized. 
- * When this function is called and later, you can perform game related operations.
- */
-export function main() {
-    console.warn("Hello world")
+function joinTeam(player, teamColor) {
+  const command = 'team join ' + teamColor;
+  server.dispatchCommand(server.getConsoleSender(), command);
+  player.sendMessage('你加入了' + teamColor + '队！');
 }
 
-/*
- * close函数将会在在PNX服务器关闭时或您的插件被卸载时调用，此时您应当进行一些清理收尾工作。
- * The close function will be called when the PNX server is shutdown or your plugin is uninstalled. 
- * At this time, you should do some cleaning and finishing work.
- */
-export function close() {
-    console.error("Goodbye world")
+function showTeamSelectionGUI(player) {
+  const form = new FormWindowSimple('团队选择', '请选择一个队伍：');
+  form.addButton(new ElementButton('红队'));
+  form.addButton(new ElementButton('蓝队'));
+
+  player.showFormWindow(form);
 }
+
+server.on('player.join', (event) => {
+  const player = event.player;
+  showTeamSelectionGUI(player);
+});
+
+server.on('form.responded', (event) => {
+  const player = event.player;
+  const response = event.response;
+
+  if (response instanceof FormWindowSimple.Response) {
+    const clickedButtonIndex = response.getClickedButtonId();
+
+    if (clickedButtonIndex === 0) {
+      joinTeam(player, 'Red');
+    } else if (clickedButtonIndex === 1) {
+      joinTeam(player, 'Blue');
+    }
+  }
+});
